30368e86432d5d4042f13702884d0e4a2fa2bce3
Dell <Dell@MacBook-Air.local>
2025-09-08 23:34:18 +0530

    web

---
+++ JS/leconeclg/index.js
@@ -0,0 +1,46 @@
+// console.log("namastee");
+
+// function exmaple(){
+//     var x =50;
+//     console.log(x);
+    
+// }
+// exmaple();
+//  console.log(x);
+
+//  function solve(){
+//     var age =25;
+//     console.log(age);
+    
+//  }
+//  solve();
+//  console.log(age);
+ 
+// let and const comes in 2015
+// {
+//    let a=20;
+//    console.log(a);
+   
+// }
+// const value1 = Symbol('hi');
+// const value2 = Symbol('hi');
+// console.log(value1);
+// console.log(value2);
+
+
+
+// console.log(`${value1} ${value2}`);
+
+
+// let arr =[1,2,3,4,5,6,7];
+// let sum = arr.reduce((a,b,c,d)=>{
+//     return a+b;
+// },10);
+// console.log(sum);
+
+let arr=[1,2,3,4,5,6,7,8,,67,6,7];
+arr.map()
+
+
+
+
