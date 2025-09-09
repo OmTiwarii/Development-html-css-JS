98bebdc9e0afd7beaa350f199ca86d4354700930
Dell <Dell@MacBook-Air.local>
2025-09-09 22:24:51 +0530

    events

---
+++ clgcar/slidechat.html/one.js
@@ -0,0 +1,18 @@
+let arr =["https://images.unsplash.com/photo-1600027901590-a341b11e8a98?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1755529582689-7a158b8f9183?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDd8Ym84alFLVGFFMFl8fGVufDB8fHx8fA%3D%3D",
+  "  https://plus.unsplash.com/premium_photo-1674500780050-dd938a43771c?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDJ8Ym84alFLVGFFMFl8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1757137910091-1cf071030691?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDR8Ym84alFLVGFFMFl8fGVufDB8fHx8fA%3D%3D" ]
+
+let b = document.querySelector('img');
+
+let num=0;
+
+setInterval(function(){
+    b.setAttribute('src',arr[num])
+    num =(num+1)%arr.length;
+},2000);
+
+
+
+
+
